3c3f70bef5bb38cfedfb8fed26c8d084f1108469
ShuVeriDA <bashtarov@outlook.com>
2022-04-04 11:48:06 +0300

    01 Count

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from '../App.module.css'
+import {Button} from "./Button";
+
+type DisplayPropsType = {
+   counter: number
+   inc: () => void
+   res: () => void
+}
+
+export const Display: React.FC<DisplayPropsType> = ({inc, res, counter}) => {
+   //1 вариант
+   // const disabledInc = counter !== 5 ? false : true
+   // const disabledRes = counter !== 0 ? false : true
+
+   // 2 вариант
+   const disabledInc = counter === 5
+   const disabledRes = counter === 0
+
+   return (
+      <div className={classes.buttons}>
+         <Button title={'inc'} callBack={inc} disabledActive={disabledInc}/>
+         <Button title={'reset'} callBack={res} disabledActive={disabledRes}/>
+      </div>
+   );
+};
